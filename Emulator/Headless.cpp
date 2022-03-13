fcfefb0a6b8dae81b6ae802bc430b3982d21e28d
mithrendal <daniel.giebener@web.de>
2022-03-13 21:12:43 +0100

    missing files and a missing makefile setting for sdl2

---
+++ Emulator/Headless.cpp
@@ -0,0 +1,187 @@
+// -----------------------------------------------------------------------------
+// This file is part of vAmiga
+//
+// Copyright (C) Dirk W. Hoffmann. www.dirkwhoffmann.de
+// Licensed under the GNU General Public License v3
+//
+// See https://www.gnu.org for license information
+// -----------------------------------------------------------------------------
+
+#include "config.h"
+#include "Headless.h"
+#include "Script.h"
+#include <getopt.h>
+
+int main(int argc, char *argv[])
+{
+    try {
+        
+        Headless().main(argc, argv);
+        
+    } catch (SyntaxError &e) {
+        
+        std::cout << "Usage: ";
+        std::cout << "vAmigaCore [-vm] <script>" << std::endl;
+        std::cout << std::endl;
+        std::cout << "       -v or --verbose   Print executed script lines" << std::endl;
+        std::cout << "       -m or --messages  Observe the message queue" << std::endl;
+        std::cout << std::endl;
+        
+        if (auto what = string(e.what()); !what.empty()) {
+            std::cout << what << std::endl;
+        }
+        
+        return 1;
+
+    } catch (VAError &e) {
+
+        std::cout << "VAError: " << std::endl;
+        std::cout << e.what() << std::endl;
+        return 1;
+        
+    } catch (std::exception &e) {
+
+        std::cout << "Error: " << std::endl;
+        std::cout << e.what() << std::endl;
+        return 1;
+    
+    } catch (...) {
+    
+        std::cout << "Error" << std::endl;
+    }
+    
+    return 0;
+}
+
+void
+Headless::main(int argc, char *argv[])
+{
+    std::cout << "vAmiga Headless v" << amiga.version();
+    std::cout << " - (C)opyright Dirk W. Hoffmann" << std::endl << std::endl;
+
+    // Parse all command line arguments
+    parseArguments(argc, argv);
+
+    // Redirect shell output to the console in verbose mode
+    if (keys.find("verbose") != keys.end()) amiga.retroShell.setStream(std::cout);
+
+    // Read the input script
+    Script script(keys["arg1"]);
+    
+    // Register message receiver
+    amiga.msgQueue.setListener(this, ::process);
+    
+    // Execute the script
+    barrier.lock();
+    script.execute(amiga);
+
+    while (!halt) {
+        
+        barrier.lock();
+        amiga.retroShell.continueScript();
+    }
+}
+
+void
+Headless::parseArguments(int argc, char *argv[])
+{
+    static struct option long_options[] = {
+        
+        { "verbose",    no_argument,    NULL,   'v' },
+        { "messages",   no_argument,    NULL,   'm' },
+        { NULL,         0,              NULL,    0  }
+    };
+    
+    // Don't print the default error messages
+    opterr = 0;
+    
+    // Remember the execution path
+    keys["exec"] = util::makeAbsolutePath(argv[0]);
+
+    // Parse all options
+    while (1) {
+        
+        int arg = getopt_long(argc, argv, ":vm", long_options, NULL);
+        if (arg == -1) break;
+
+        switch (arg) {
+                
+            case 'v':
+                keys["verbose"] = "1";
+                break;
+
+            case 'm':
+                keys["messages"] = "1";
+                break;
+
+            case ':':
+                throw SyntaxError("Missing argument for option '" +
+                                  string(argv[optind - 1]) + "'");
+                
+            default:
+                throw SyntaxError("Invalid option '" +
+                                  string(argv[optind - 1]) + "'");
+        }
+    }
+    
+    // Parse all remaining arguments
+    auto nr = 1;
+    while (optind < argc) {
+        keys["arg" + std::to_string(nr++)] = util::makeAbsolutePath(argv[optind++]);
+    }
+    
+    checkArguments();
+}
+
+void
+Headless::checkArguments()
+{
+    // The user needs to specify a single input file
+    if (keys.find("arg1") == keys.end()) {
+        throw SyntaxError("No script file is given");
+    }
+    if (keys.find("arg2") != keys.end()) {
+        throw SyntaxError("More than one script file is given");
+    }
+        
+    // The input file must exist
+    if (!util::fileExists(keys["arg1"])) {
+        throw SyntaxError("File " + keys["arg1"] + " does not exist");
+    }
+}
+
+void
+process(const void *listener, long type, u32 data1, u32 data2)
+{
+    ((Headless *)listener)->process(type, data1, data2);
+}
+
+void
+Headless::process(long type, u32 data1, u32 data2)
+{
+    static bool messages = keys.find("messages") != keys.end();
+    
+    if (messages) {
+        
+        std::cout << MsgTypeEnum::key(type);
+        std::cout << "(" << data1 << ", " << data2 << ")" << std::endl;
+    }
+        
+    switch (type) {
+            
+        case MSG_SCRIPT_DONE:
+        case MSG_SCRIPT_ABORT:
+        case MSG_ABORT:
+
+            halt = true;
+            [[fallthrough]];
+            
+        case MSG_SCRIPT_WAKEUP:
+
+            barrier.unlock();
+            break;
+ 
+        default:
+            break;
+    }
+}
