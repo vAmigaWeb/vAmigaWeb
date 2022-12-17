758d0169b38e0c0e6bd9851bdef8a7f3e36ffde4
mithrendal <daniel.giebener@web.de>
2022-12-17 23:29:11 +0100

    more work on file dialog and auto mount_list

--- js/vAmiga_ui.js
+++ js/vAmiga_ui.js
@@ -772,12 +772,7 @@ function configure_file_dialog(reset=false)
         else
         {
             $("#file_slot_dialog_label").html(" "+file_slot_file_name);
-            //configure file_slot
-
-            var auto_load = false;
-            var auto_press_play = false;
-            var auto_run = false;
-            
+            //configure file_slot  
             $("#button_insert_file").removeAttr("disabled");
             $("#div_zip_content").hide();
             $("#button_eject_zip").hide();
@@ -889,7 +884,7 @@ function configure_file_dialog(reset=false)
                                 {
                                     let drive_number= df_mount_list.indexOf(path);
                                     if(drive_number<0)
-                                        hd_mount_list.indexOf(path);
+                                        drive_number=hd_mount_list.indexOf(path);
                                     file_slot_file_name=path;
                                     file_slot_file = await zip.file(path).async("uint8array");
                                     insert_file(drive_number);
@@ -906,12 +901,7 @@ function configure_file_dialog(reset=false)
                 $("#button_insert_file").html("mount file"+return_icon);
                 $("#button_insert_file").attr("disabled", true);
             }
-
-            $("#auto_load").prop('checked', auto_load);
-            $("#auto_press_play").prop('checked', auto_press_play);
-            $("#auto_run").prop('checked', auto_run);    
-
-            if(file_slot_file_name.match(/[.](adf|hdf|dms|exe|vAmiga)$/i))
+            else if(file_slot_file_name.match(/[.](adf|hdf|dms|exe|vAmiga)$/i))
             {
                 insert_file();
             }
@@ -2429,12 +2419,6 @@ $('.layer').change( function(event) {
     reset_before_load=false;
     insert_file = function(drive=0) 
     {   
-        if($('#div_zip_content').is(':visible'))
-        {
-            configure_file_dialog(reset_before_load);
-            return;
-        }
-        
         $('#modal_file_slot').modal('hide');
 
         var execute_load = async function(drive){
@@ -2479,7 +2463,7 @@ $('.layer').change( function(event) {
         else
         {
             setTimeout(async ()=> {
-                await execute_load();
+                await execute_load(drive);
                 wasm_reset();
                 if(call_param_warpto !=null){
                     wasm_configure("warp_to_frame", `${call_param_warpto}`);
