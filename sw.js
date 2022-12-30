e35e085b532ed12641106e5947a760ba1ef227b6
mithrendal <daniel.giebener@web.de>
2022-12-30 18:52:25 +0100

    version

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const url_root_path= self.location.pathname.replace("/sw.js","");
 const core_version  = '2.3b1'; //has to be the same as the version in Emulator/config.h
-const ui_version = '2022_12_28'+url_root_path.replace("/","_");
+const ui_version = '2022_12_30'+url_root_path.replace("/","_");
 const cache_name = `${core_version}@${ui_version}`;
 const settings_cache = 'settings';
 
