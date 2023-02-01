22b16a47eb3216b62581d98688bed9e420595194
mithrendal <mithrendal@web.de>
2023-02-01 22:52:15 +0100

    made load eruda.js fromo same origin ... needed by the web worker built

--- js/vAmiga_ui.js
+++ js/vAmiga_ui.js
@@ -2448,7 +2448,7 @@ $('.layer').change( function(event) {
 });
 
 //------
-    load_console=function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda@2.4.1"; document.body.appendChild(script); script.onload = function () { eruda.init(
+    load_console=function () { var script = document.createElement('script'); script.src="js/eruda.js"; document.body.appendChild(script); script.onload = function () { eruda.init(
     {
         defaults: {
             displaySize: 50,
