e606befb051fe5084b2479580ec8a9bc7acd26eb
mithrendal <mithrendal@web.de>
2024-05-27 21:39:34 +0200

    fixed auto save snapshot which was broken

--- sw_template.js
+++ sw_template.js
@@ -1,6 +1,6 @@
 const url_root_path= self.location.pathname.replace("/sw.js","");
 const core_version  = '2.6.0'; //has to be the same as the version in Emulator/config.h
-const ui_version = '2024_05_26'+url_root_path.replace("/","_");
+const ui_version = '2024_05_27'+url_root_path.replace("/","_");
 const needs_shared_array_buffer=@needs_shared_array_buffer@; //true when vAmiga runs in separat worker thread
 const cache_name = `${core_version}@${ui_version}`;
 const settings_cache = 'settings';
