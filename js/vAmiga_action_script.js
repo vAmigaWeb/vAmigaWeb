97f1f864bfc9bdb9b10be8b28194368ff2d6c807
mithrendal <mithrendal@web.de>
2024-06-04 22:37:20 +0200

    added system actions for activity monitor, hide/show all actionbuttons, toggle warp

--- js/vAmiga_action_script.js
+++ js/vAmiga_action_script.js
@@ -224,6 +224,15 @@ async function execute_single_action(cmd, execute=true, execution_id=-1)
             $('#button_run').click();   
         }
     }
+    else if(cmd == 'toggle_warp')
+    {
+        if(execute)
+        {//0==auto, 1==never, 2==always
+            let mode = wasm_get_config_item("WARP_MODE");
+            if(mode <= 1) auto_or_never_warp_mode = mode ==0 ?"WARP_AUTO":"WARP_NEVER";
+            wasm_configure('WARP_MODE', mode<=1 ? 'WARP_ALWAYS': auto_or_never_warp_mode);
+        }
+    }
     else if(cmd == 'warp_always')
     {
         if(execute)
@@ -355,6 +364,24 @@ async function execute_single_action(cmd, execute=true, execution_id=-1)
             await action_button_released(execution_id);
         }
     }
+    else if(cmd == 'activity_monitor')
+    {
+        if(execute)
+        {
+            if(document.querySelector("#activity"))
+                hide_activity();
+            else
+                show_activity();
+        }
+    }
+    else if(cmd == 'toggle_action_buttons')
+    {
+        if(execute)
+        {
+            let this_buttons_label = $('#ck'+execution_id).text();
+            $(".custom_key").not(`":contains('${this_buttons_label}')"`).toggle();
+        }
+    }    
     else
     {
         valid=false;
