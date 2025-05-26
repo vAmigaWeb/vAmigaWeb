9e5fa7c36545f4a536ea3f4cb5db32b62644b664
mithrendal <mithrendal@web.de>
2025-05-26 22:18:28 +0200

    version 4.1.2

--- Emulator/VAmigaConfig.h
+++ Emulator/VAmigaConfig.h
@@ -16,13 +16,13 @@
 // Version number
 static constexpr int VER_MAJOR      = 4;
 static constexpr int VER_MINOR      = 1;
-static constexpr int VER_SUBMINOR   = 1;
+static constexpr int VER_SUBMINOR   = 2;
 static constexpr int VER_BETA       = 0;
 
 // Snapshot version number
 static constexpr int SNP_MAJOR      = 4;
 static constexpr int SNP_MINOR      = 1;
-static constexpr int SNP_SUBMINOR   = 1;
+static constexpr int SNP_SUBMINOR   = 2;
 static constexpr int SNP_BETA       = 0;
 
 
