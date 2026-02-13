f9ace779f01847cc8fa394323a07d26f3e810c94
mithrendal <mithrendal@web.de>
2026-02-13 22:28:12 +0100

    css work on player

--- js/vAmigaWeb_player.js
+++ js/vAmigaWeb_player.js
@@ -146,7 +146,7 @@
 <div id="player_container" style="display:flex;flex-direction:column;">
 <iframe id="vAmigaWeb" width="100%" height="100%" src="${this.vAmigaWeb_url}${params}#${address}">
 </iframe>
-<div style="display:grid;grid-template-columns: repeat(5, auto) 3fr repeat(3, auto);grid-gap: 0.25em;">
+<div style="display:grid;grid-template-columns: repeat(5, auto) 3fr repeat(3, auto);grid-gap: 0.25em;text-align:start">
 <svg id="stop_icon" class="player_icon_btn" onclick="vAmigaWeb_player.stop_emu_view();return false;" xmlns="http://www.w3.org/2000/svg" width="2.0em" height="2.0em" fill="currentColor" class="bi bi-pause-btn" viewBox="0 0 16 16">
     <path d="M6.5 5A1.5 1.5 0 0 0 5 6.5v3A1.5 1.5 0 0 0 6.5 11h3A1.5 1.5 0 0 0 11 9.5v-3A1.5 1.5 0 0 0 9.5 5h-3z"/>
     <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
