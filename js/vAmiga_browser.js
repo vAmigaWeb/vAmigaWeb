338c1d86d924bd0e130565a8edf448b88869d246
mithrendal <mithrendal@web.de>
2026-08-03 20:49:07 +0200

    set version to 4.3.5 (with AGA support)

--- js/vAmiga_browser.js
+++ js/vAmiga_browser.js
@@ -1,5 +1,5 @@
-const vAmigaWeb_version="4.3.1"; //minimum requirement for snapshot version to be compatible
-const compatible_snapshot_version_format=/^(4[.]3[.]1)$/g
+const vAmigaWeb_version="4.3.5"; //minimum requirement for snapshot version to be compatible
+const compatible_snapshot_version_format=/^(4[.]3[.]5)$/g
 var current_browser_datasource='snapshots';
 var current_browser_command=null;
 
